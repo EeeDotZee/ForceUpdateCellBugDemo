9e6d598af13b6315c0845a44771f50548776ae7d
Elias Grauß <elias.grauss@gmail.com>
2019-08-01 07:33:22 +0200

    Init

---
+++ ForceUpdateSizeDemo/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ForceUpdateSizeDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            Task.Delay(2000);
+         
+        }
+
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            if(AppearingCell.Height > 0)
+            {
+                AppearingCell.Height = 0;
+            }
+            else
+            {
+                AppearingCell.Height = 42;
+            }
+            AppearingCell.ForceUpdateSize();
+        }
+    }
+}
